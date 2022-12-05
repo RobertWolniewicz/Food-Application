5b8f96e722c9b007462e6ded1625046e41388559
Robert Wolniewicz <robert_wol@wp.pl>
2022-12-05 16:27:00 +0100

    Revert "Revert "Add project files.""
    This reverts commit 050a62c40e87d9b6f7029a3db5d7aa90b4050970.

---
+++ Food Application/DishOptions/Deleter.cs
@@ -0,0 +1,13 @@
+﻿namespace Food_Application.DishOptions
+{
+    internal class Deleter
+    {
+        public static async Task Delet(ApplicatioDbContext dbContext, String DishToDelet)
+        {
+            var deletedDish = await dbContext.Dishs.FirstAsync(D => D.name == DishToDelet);
+            dbContext.Dishs.Remove(deletedDish);
+            dbContext.SaveChangesAsync();
+            return;
+        }
+    }
+}
---
+++ Food Application/DishOptions/Updater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food_Application.DishOptions;
+
+internal class Updater
+{
+    public static async void Update(Form3 form, ApplicatioDbContext DbContext, string updatingDish)
+    {
+        var Dish = await DbContext.Dishs.FirstAsync(D => D.name.ToLower() == updatingDish.ToLower());
+            Dish.name = form.textBox1.Text;
+            Dish.portions = double.Parse(form.textBox2.Text);
+        foreach (var ingridien in form.ingridienList)
+        {
+            Dish.ingridiens.Add(ingridien);
+        }
+        await DbContext.SaveChangesAsync();
+    }
+}
